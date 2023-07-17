4bdc250c8c6e9dd557f59d153547d3f56285780f
Simon Helmig <40271543+helmigsimon@users.noreply.github.com>
2023-07-17 08:00:23 +0100

    Continuing work on decoder

--- computer_systems/exercises/00_Protobuf_varint/varint/solution.py
+++ computer_systems/exercises/00_Protobuf_varint/varint/solution.py
@@ -8,16 +8,25 @@ PATH_TO_INT_MAP = {
 
 
 def main() -> None:
-    for path, _int in PATH_TO_INT_MAP.items():
-        res = get_integer_from_binary_file(path)
-        print(res)
-        assert _int == res, f"{_int} != {res}"
+    #for path, _int in PATH_TO_INT_MAP.items():
+    #    res = get_integer_from_binary_file(path)
+    #    print(res)
+    #    assert _int == res, f"{_int} != {res}"
 
-        encoded_res = encode(res)
-        print(encoded_res)
+    #    encoded_res = encode(res)
+    #    print(encoded_res)
 
-        decoded_res = decode(encoded_res)
-        print(decoded_res)
+    #    decoded_res = decode(encoded_res)
+    #    print(decoded_res)
+
+        #for i in range(1, int(1e9)):
+        #if i % 1000 == 0:
+        #    print(f'Checking {i}')
+    i = 16384
+    try:
+        assert decode(encode(i)) == i
+    except Exception:
+        import pdb;pdb.set_trace()
 
 
 def encode(num: int) -> None:
@@ -51,13 +60,44 @@ HEX_TO_INT_MAP = {
 }
 
 def decode(repr: bytes) -> int:
+    import pdb;pdb.set_trace()
+    decoded = []
     hex = repr.hex()
-    while hex:
-        hex_part = int(hex[:2])
-    return hex_part
+    assert len(hex) % 2 == 0
+    hex_bin = hextobin(hex)
+    assert len(hex_bin) % 8 == 0
+    indices = _get_dropped_msb_sequence(len(hex_bin) // 8)
+    dropped_msb = ''.join([
+        hex_bin[i:j] for i,j in zip(indices, indices[1:] + [None])
+    ])
+    indices = [7,15,0,7]
+    big_endian = dropped_msb[8:15] + dropped_msb[0:7]
+    return int(big_endian,2)
 
 def _convert_hex_to_int(hex: str, pos: int) -> int:
     return HEX_TO_INT_MAP[hex] * 16**pos
+
+def _get_dropped_msb_sequence(n_bytes: int):
+    return _get_bit_sequence(n_bytes, 8)
+
+
+def _get_bit_sequence(n_bytes: int, bits: int) -> list[int]:
+    res = []
+    for i in range(n_bytes):
+        res.extend([1+bits*i,bits+bits*i]) 
+    return res
+
+
+def _convert_to_big_endian(bin_str: str, bits: int) -> int:
+    bit_sequence = _get_bit_sequence(len(bin_str) // bits, bits)
+    res = []
+    while bit_sequence:
+        begin, end = bit_sequence.pop(), bit_sequence.pop()
+        res.insert(end,0)
+        res.insert(begin,0)
+    return res
+        
+
 
 
 def get_integer_from_binary_file(path: str) -> int:
@@ -69,6 +109,11 @@ def get_integer_from_binary_file(path: str) -> int:
 def _interpret_hex_string_as_int(hex: bytes) -> int:
     return struct.unpack(">Q", hex)[0]
 
+def hextobin(hs: str):
+    try:
+        return bin(int(hs, 16))[2:].zfill(len(hs) *4)
+    except:
+        import pdb;pdb.set_trace()
 
 def read_integer_from_file(file: str) -> bytes:
     with open(file, "rb") as f:
