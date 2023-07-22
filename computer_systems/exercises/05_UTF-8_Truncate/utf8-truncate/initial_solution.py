cea198ad3a02eb9d89a92031543a3b45a028af35
Simon Helmig <40271543+helmigsimon@users.noreply.github.com>
2023-07-22 15:31:59 +0200

    Extending explanation of how to solve using decimal in initial solution

--- computer_systems/exercises/05_UTF-8_Truncate/utf8-truncate/initial_solution.py
+++ computer_systems/exercises/05_UTF-8_Truncate/utf8-truncate/initial_solution.py
@@ -2,9 +2,21 @@ import sys
 
 
 def find_cutoff_point(line: bytes, trunc: int) -> int:
-    if trunc <= 0 or trunc >= len(line):
+    if trunc >= len(line):
         return trunc
-    if (line[trunc] & 0xc0 == 0x80):
+    ## Why are the two conditions below not equivalent for all values?
+    ## It is true that all unicode characters will be larger than 128
+    ## But we are only interested in setting our pointer backwards
+    ## if we have landed at a continuation byte
+    ## A starting byte (that indicates length of utf-8 encoding)
+    ## Will also be > 128, but for these we don't necessarily want to 
+    ## continue truncating
+    ## A decimal alternative would be x > 128 and x < 191
+    ##                                x > 0xC0 and x < 0xef
+    if not (line[trunc] & 0xc0 == 0x80) and (line[trunc] > 128):
+        pass
+    #if (line[trunc] & 0xc0 == 0x80):
+    if (line[trunc] > 128 and line[trunc] <= 191):
         return find_cutoff_point(line, trunc - 1)
     return trunc
 
