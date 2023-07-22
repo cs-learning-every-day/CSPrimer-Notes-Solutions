e5c4dbf4cc5cafa039fdae3faf763b69bee1f90d
Simon Helmig <40271543+helmigsimon@users.noreply.github.com>
2023-07-22 15:02:38 +0200

    Finishing initial solution and adding ozs solution

--- computer_systems/exercises/05_UTF-8_Truncate/utf8-truncate/solution.py
+++ computer_systems/exercises/05_UTF-8_Truncate/utf8-truncate/solution.py
@@ -1,28 +1,39 @@
-import os
+"""
+- Parse Test cases
+    - Read Lines
+    - uint8 for n
+    - remember to strip trailing \n
+- Truncation
+    - Start from end
+    - if s[n] starts with 0b10 then n -=1
+    - Remember to account for n out of bounds
+- Finally
+    - Print to standard out
+    - Diff against expected
+"""
 import sys
-import re
-import pdb
 
-OUTPUT_FILE = 'output'
 
-if OUTPUT_FILE in os.listdir():
-    os.remove(OUTPUT_FILE)
+def truncate(string: bytes, num: int) -> bytes:
+    if num >= len(string):
+        return string
+    import pdb;pdb.set_trace()
+    # If byte at trunation piont starts with 10 specifically then we move pointer down
+    # Finding if byte starts with 10
+        # Mask out the first two bits - by bitwise and on 0b11000000 (or 0xC0)
+        # Check if they are equivalent to 0b10000000 (or 0x80)
+    while num > 0 & (string[num] & 0xc0 == 0x80):
+        num -= 1
+    return string[:num]
 
-output_file = open(OUTPUT_FILE, 'wb')
 
 with open('cases', 'rb') as f:
-    while f.peek():
+    while True:
         line = f.readline()
-        first_byte, rest_of_line = line[0], line[1:]
-        cutoff_point = first_byte
-        if cutoff_point <= len(rest_of_line):
-            while rest_of_line[cutoff_point-1] > 128 and cutoff_point > 0:
-                cutoff_point -= 1
-        truncated_line = rest_of_line[:cutoff_point]
-        if truncated_line != rest_of_line:
-            truncated_line += b'\n'
-        output_file.write(truncated_line)
-
-
-output_file.close()
+        if len(line) == 0:
+            break
+        n = line[0]
+        s = line[1:-1]
+        res = truncate(s, n)
+        sys.stdout.buffer.write(res + b'\n')
 
