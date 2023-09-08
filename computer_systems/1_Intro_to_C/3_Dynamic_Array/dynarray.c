3213c1a0a116dcb569b6a3fe8fa8196f7b1673cf
Simon Helmig <40271543+helmigsimon@users.noreply.github.com>
2023-09-08 08:28:08 +0100

    Working implementation of dynarray.c without DA_free

--- computer_systems/1_Intro_to_C/3_Dynamic_Array/dynarray.c
+++ computer_systems/1_Intro_to_C/3_Dynamic_Array/dynarray.c
@@ -26,7 +26,6 @@ typedef struct DA {
 
 
 DA* DA_new (void) {
-  // TODO allocate and return a new dynamic array
   DA* da = (DA*)malloc(sizeof(DA));
   if (!da){
     perror("Failed to allocate memory");
@@ -43,15 +42,13 @@ DA* DA_new (void) {
 }
 
 int DA_size(DA *da) {
-  // TODO return the number of items in the dynamic array
   return da->size;
 }
 
 void DA_push (DA* da, void* x) {
-  // TODO push to the end
   void* new_val = x;
   if (da -> size >= da -> capacity){
-    int new_capacity = da->capacity+1;
+    int new_capacity = da->size+1;
     void **temp = realloc(da -> values, sizeof(void*) * new_capacity);
     if (!temp){
       perror("Failed to allocate memory");
@@ -61,38 +58,30 @@ void DA_push (DA* da, void* x) {
     da -> values = temp;
     da -> capacity = new_capacity;
   }
-  for (int i=0; i<da->capacity; i++){
-    void* old_val = da -> values[i];
-    da -> values[i] = new_val;
-    new_val = old_val;
-  }
-  (da->size)++;
+  da -> values[da->size] = x;
+ (da->size)++;
 }
 
 void *DA_pop(DA *da) {
-  // TODO pop from the end
+  if (da->size == 0) return NULL;
   int array_size = da -> capacity;
-  void* return_val = (da -> values)[0];
-  for (int i=0;i<array_size;i++){
-    (da -> values)[i] = (da -> values[i+1]);
-  }
+  void* return_val = (da -> values)[da->size-1];
+  da->values[da->size-1] = NULL;
   da -> size--;
   return return_val;
 }
 
 void DA_set(DA *da, void *x, int i) {
-  // TODO set at a given index, if possible
   int array_size = da -> capacity;
   if (i+1 >= array_size) return;
   void* value = da -> values[i];
   da -> values[i] = x;
-  if (value != NULL) {(da -> size)++;};
+  if (value == NULL) {(da -> size)++;};
 }
 
 void *DA_get(DA *da, int i) {
-  // TODO get from a given index, if possible
   int array_size = da -> capacity;
-  if (i+1 >= array_size) return NULL;
+  if (i+1 > array_size) return NULL;
   return da -> values[i];
 }
 
@@ -110,11 +99,9 @@ int main() {
     // basic push and pop test
     int x = 5;
     float y = 12.4;
-    printf("first val: %p\n", &x);
 
     DA_push(da, &x);
     DA_push(da, &y);
-    printf("size: %d", DA_size(da));
     assert(DA_size(da) == 2);
 
     assert(DA_pop(da) == &y);
@@ -141,7 +128,7 @@ int main() {
     }
     assert(DA_size(da) == n);
     for (i = 0; i < n; i++) {
-      assert(DA_get(da, i) == &arr[i]);
+      assert(DA_get(da,i) == &arr[i]);
     }
     for (; n; n--)
       DA_pop(da);
