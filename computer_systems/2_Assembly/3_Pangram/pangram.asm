2fe942dd1b2f1511ec20483029e3ffc34dae88b3
Simon Helmig <40271543+helmigsimon@users.noreply.github.com>
2023-12-29 17:21:35 +0100

    Continuing to try to convert c program to assembly

--- computer_systems/2_Assembly/3_Pangram/pangram.asm
+++ computer_systems/2_Assembly/3_Pangram/pangram.asm
@@ -2,17 +2,30 @@ section .text
 global pangram
 pangram:
 	mov rsi, 0 ; Create bitset
-	mov al, [rdi] ; Read first byte from rdi
-	cmp al, 0; Compare to 0
-	jz res; If 0, jump to exit
+	mov r10, 0x17f ; Move value into r10 for masking
+	mov r11, 0x07fffffe ; MOve value into r11 for masking
 
-res:
-	cmp rsi, 0x03ffffff
-	jz decline
-	mov eax, 0
-	ret
+
+loop: 
+	mov rax, [rdi] ; Read first byte from rdi
+	cmp rax, 0; Compare to 0
+	jz res; If 0, jump to exit
+	cmp rax, 64 ; Ignore first 64 chars of ascii
+	jl loop ; Take next byte if is any lower than 64
+	and rax, r10 ; Bitwise and of al register to mask out 0x1f
+	mov r9, 1 ; Put 1 into an empty register
+	shl r9, rax ; Shift 1 by number of bits in al
+	or rsi, r9 ; Bitwise or of bitset and result in r9
+	and rsi, r11 ; Mask out lower bit values
+	cmp rsi, r11 ; Compare to mask
+	jz res ; If equal to mask - then return 0 else 1
 
 decline:
 	mov eax, 1
 	ret ; 
+
+res:
+	mov eax, 0
+	ret
+
 	
