3a6621e44d7e15d0a258c166dd96371c95e0761f
Simon Helmig <40271543+helmigsimon@users.noreply.github.com>
2024-01-02 21:25:53 +0000

    Making some progress on finishing the translation of pangram

--- computer_systems/2_Assembly/3_Pangram/pangram.asm
+++ computer_systems/2_Assembly/3_Pangram/pangram.asm
@@ -1,31 +1,37 @@
 section .text
 global pangram
 pangram:
-	mov rsi, 0 ; Create bitset
-	mov r10, 0x17f ; Move value into r10 for masking
-	mov r11, 0x07fffffe ; MOve value into r11 for masking
+	mov esi, 0 ; Create bitset
+	mov dl, 0x1f ; Move value into dl for masking
+	mov r11d, 0x07fffffe ; MOve value into r11d for masking
 
+looping: 
+	mov al, [rdi] ; Read first byte from rdi
+	cmp al, 0; Compare byte to 0
+	jz masking; If 0, jump to exit
+	cmp al, 0x40 ; Ignore first 64 chars of ascii
+	jl looping ; Take next byte if it is any lower than 64
+	and al, dl ; Bitwise and of al register to mask out 0x1f
+	mov cl, al ; Move result of comparison into cl register
+	mov rbp, 0 ; Zero out ecx
+	mov bpl, 0x01 ; Put 1 into an empty register
+	shl ebp, cl ; Shift 1 by number of bits in al
+	or esi, ebp ; Bitwise or of bitset and result in r9
+	add rdi, 0x01 ; Increment rdi by 1 bit
+	jmp looping ; Continue Looping 
 
-loop: 
-	mov rax, [rdi] ; Read first byte from rdi
-	cmp rax, 0; Compare to 0
-	jz res; If 0, jump to exit
-	cmp rax, 64 ; Ignore first 64 chars of ascii
-	jl loop ; Take next byte if is any lower than 64
-	and rax, r10 ; Bitwise and of al register to mask out 0x1f
-	mov r9, 1 ; Put 1 into an empty register
-	shl r9, rax ; Shift 1 by number of bits in al
-	or rsi, r9 ; Bitwise or of bitset and result in r9
-	and rsi, r11 ; Mask out lower bit values
-	cmp rsi, r11 ; Compare to mask
-	jz res ; If equal to mask - then return 0 else 1
-
-decline:
-	mov eax, 1
-	ret ; 
+masking:
+	and esi, r11d ; Mask out lower bit values
+	cmp esi, r11d ; Compare to mask
+	jz decline ; If equal to mask - then return 0 else 1
 
 res:
 	mov eax, 0
 	ret
+
+
+decline:
+	mov eax, 1
+	ret ; 
 
 	
