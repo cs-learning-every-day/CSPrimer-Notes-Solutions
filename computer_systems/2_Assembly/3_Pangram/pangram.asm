09a2e77a270e836bc1014c7f5648c3df7af00436
Simon Helmig <40271543+helmigsimon@users.noreply.github.com>
2024-01-05 09:03:07 +0000

    Reformatting code

--- computer_systems/2_Assembly/3_Pangram/pangram.asm
+++ computer_systems/2_Assembly/3_Pangram/pangram.asm
@@ -2,6 +2,9 @@ section .text
 global pangram
 
 pangram:
+	call _prep
+
+_prep: 
 	xor rsi, rsi ; Create bitset
 	xor rax, rax ; Prep - zero out rax
 	xor rcx, rcx ; Prep - Zero out rcx
@@ -10,12 +13,12 @@ pangram:
 	mov dl, 0x1f ; Move value into dl for masking
 	mov r11d, 0x07fffffe ; MOve value into r11d for masking
 
-looping: 
+_looping: 
 	mov al, [rdi] ; Read first byte from rdi
 	cmp al, 0x00; Compare byte to 0
-	jz masking; If 0, jump to exit
+	jz _exit; If 0, jump to exit
 	cmp al, 0x40 ; Ignore first 64 chars of ascii
-	jl looping ; Take next byte if it is any lower than 64
+	jl _looping ; Take next byte if it is any lower than 64
 	and al, dl ; Bitwise and of al register to mask out 0x1f
 	mov cl, al ; Move result of comparison into cl register
 	mov rbp, 0 ; Zero out rbp
@@ -23,19 +26,23 @@ looping:
 	shl ebp, cl ; Shift 1 by number of bits in al
 	or esi, ebp ; Bitwise or of bitset and result in r9
 	add rdi, 0x01 ; Increment rdi by 1 bit
-	jmp looping ; Continue Looping 
+	jmp _looping ; Continue Looping 
 
-masking:
+_exit:
 	and esi, r11d ; Mask out lower bit values
 	cmp esi, r11d ; Compare to mask
+	sete eax ;
 	jz decline ; If equal to mask - then return 0 else 1
 
-res:
+_exit: 
+	
+
+_res:
 	mov eax, 0
 	ret
 
 
-decline:
+_decline:
 	mov eax, 1
 	ret ; 
 
