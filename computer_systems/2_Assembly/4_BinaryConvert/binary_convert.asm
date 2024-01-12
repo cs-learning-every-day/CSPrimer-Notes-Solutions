64aeb960ff4875146d7dcfcc4ecf03d4973a5d2d
Simon Helmig <40271543+helmigsimon@users.noreply.github.com>
2024-01-12 08:57:35 +0000

    Finishing binary convert

--- computer_systems/2_Assembly/4_BinaryConvert/binary_convert.asm
+++ computer_systems/2_Assembly/4_BinaryConvert/binary_convert.asm
@@ -2,7 +2,33 @@ section .text
 global binary_convert
 binary_convert:
 	xor eax, eax ; zero out eax
+	xor ebx, ebx ; zeor out ebx
+	xor ecx ,ecx ; zero out ecx
+
+.find_number_of_points:
+	movzx edx, byte [rdi] ; read byte from RDI
+	cmp edx, 0
+	je .reset_rdi
+	add ecx, 1
+	add rdi, 1
+	jmp .find_number_of_points
+
+.reset_rdi:
+	sub rdi, rcx
+	sub ecx, 1
+
+
+.loop:
 	movzx edx, byte [rdi] ; Read byte from RDI
-	cmp edx, 0x31
-	sete al
+	cmp edx, 0
+	je .exit
+	cmp edx, '1'
+	sete bl
+	shl ebx, cl
+	add eax, ebx
+	sub cl, 1
+	add rdi, 1
+	jmp .loop
+
+.exit:
 	ret
