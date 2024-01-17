f24dbfb370c04c21326a8e652f988915c77ef992
Simon Helmig <40271543+helmigsimon@users.noreply.github.com>
2024-01-17 08:26:48 +0000

    Completed ConeVolume

--- computer_systems/2_Assembly/5_ConeVolume/cone.asm
+++ computer_systems/2_Assembly/5_ConeVolume/cone.asm
@@ -3,13 +3,23 @@ default rel
 section .text
 global volume
 
-section .data
-    data            dq 3.14159
+;section .data
+;    const            dd 3.14159
+
 volume:
-    mov edx, 3
-    movlpd  xmm3, [data]
-    cvtsi2sd xmm2, edx
-    mulsd xmm0, xmm1
-    mulsd xmm0, xmm3
-    divsd xmm0, xmm2
+    ;; Load 3 into xmm3
+    mov edx, 3 
+    ;; Loading into xmm2 as a 32 bit float
+    cvtsi2ss xmm2, edx
+    ;; Load pi into xmm3 as a 32 bit float
+    mov eax,__?float32?__(3.14159)
+    movd xmm3,eax
+    ;; Square the radius - mulss multiplies 32 bit floats
+    mulss xmm0, xmm0
+    ;; Multiply by pi
+    mulss xmm0, xmm3
+    ;; Multiply by height
+    mulss xmm0, xmm1
+    ;; Divide by 3
+    divss xmm0, xmm2
     ret
--- computer_systems/2_Assembly/5_ConeVolume/tests.c
+++ computer_systems/2_Assembly/5_ConeVolume/tests.c
@@ -10,7 +10,7 @@ int main(void) {
   assert(float_near(0.0f, volume(0.0f, 0.0f)));
   // Stored in xmm0 & xmm1, in little endian format
   // Need to be reversed to be encoded 
-  assert(float_near(2.09f, volume(1.5f, 2.5f)));
+  assert(float_near(5.89f, volume(1.5f, 2.5f)));
   assert(float_near(174.23f, volume(5.5f, 5.5f)));
   assert(float_near(9.05f, volume(1.234f, 5.678f)));
   printf("OK\n");
