78067b495edfad972978c2c4416fe203f5e8404f
Simon Helmig <40271543+helmigsimon@users.noreply.github.com>
2024-03-25 08:51:56 +0100

    Naive Go Solution

---
+++ algorithms/1_ProblemSolving/II_ConvertToRoman/attempt.py
@@ -0,0 +1,23 @@
+"""
+In this problem, you will write a short function to convert an integer to its Roman numeral form. 
+If you start coding without a plan, it's likely that you will end up with an unnecessarily complicated approach!
+
+
+The Problem:
+- Input
+    - Integer
+- Output:
+    - String
+- Parameters
+    - Roman Numerals
+        - M -> 1000
+        - D -> 500
+        - C -> 100
+        - L -> 50
+        - X -> 10
+        - V -> 5
+        - I -> 1
+
+
+Plan:
+"""
