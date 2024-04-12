7dc44b8276c180bda0d8080fff472abc46c01e57
Simon Helmig <40271543+helmigsimon@users.noreply.github.com>
2024-04-12 08:33:16 +0100

    Finishing calculator and starting on linked list

---
+++ algorithms/3_LinearStructures/2_DoublyLinkedList/main.go
@@ -0,0 +1,25 @@
+/*
+- Linked Lists are a good introduction to trees & graphs
+- Linked Lists themselves are not a great data structure
+    - They're a one-branch tree - a trunk
+- Deque (double-ended queue)
+    - You want something like a stack
+    - But you also want to access it from the beginning
+    - Potential use-case: Undo Buffer
+
+Plan:
+- Interface:
+    - 
+
+*/
+
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    fmt.Println("Hello, world!")
+}
+
