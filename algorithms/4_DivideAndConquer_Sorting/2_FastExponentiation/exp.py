d53e722cc56f86e4eeadd8c2b9007f4da3ceac7e
Simon Helmig <40271543+helmigsimon@users.noreply.github.com>
2024-04-17 20:30:20 +0100

    Final touches to exponentiation and starting on Quicksort

--- algorithms/4_DivideAndConquer_Sorting/2_FastExponentiation/exp.py
+++ algorithms/4_DivideAndConquer_Sorting/2_FastExponentiation/exp.py
@@ -28,11 +28,84 @@ def fast_exp(a, n):
     return res * res
     
 
+def fast_exp_iter_sol(a, n):
+    res = 1
+    while n > 0:
+        if n % 2 == 1:
+            res *= a
+            n -= 1
+        else:
+            a *= a
+            n //= 2
+    return res
+
+
+
+
+
+
 def fast_exp_iter(a,n):
-    """TODO: Implement iteratively"""
+    """TODO: Implement iteratively
+    - We want to implement iteratively to avoid:
+        - Function calling overhead
+        - Stack depth (there's a limited number of stack frames we can use)
+        - In practice:
+            - This isn't actually a problem for us, b/c even to the power of a billion will take ~30 ops
+        - In theory:
+            - Could be a problem in security and number theoretic applications, where you might have insane exponents
+
+    Understand:
+        - What do we need to iterate over?
+            - If we iterate over length of n -> O(n)
+        - We need an iterative divide and conquer approach
+            - I.e. we need to reduce the size of the problem each time
+        - Start from the base case, iteratively run the same logic but in reverse
+            - We can't square our accumulator as we go
+            - We'll square the power as we go
+                - 3 ** 8 = (3 ** 2) ** 4
+
+    Plan:
+        - if n == 0:
+            retrun 1
+        - if n == 1:
+            return a
+        - acc = a * a
+        - power = 2
+        - while power <= n:
+            acc *= acc
+            power *= 2
+        - if power == n:
+            return acc
+        - while power > n:
+            acc /= a
+            power -= 1
+          return acc
+
+
+    """
+    if n == 0:
+        return 1
+    if n == 1:
+        return a
+    acc = a * a
+    power = 2
+    while power <= n:
+        acc *= acc
+        power *= 2
+    while power > n:
+        acc //= a
+        power -= 1
+    return acc
 
 
 
 if __name__ == "__main__":
-    fast_exp(3, 100000)
+    TEST_CASES = [
+        (3, 1000),
+        (4, 1022)
+    ]
+    for a, n in TEST_CASES:
+        assert fast_exp(a, n) == a ** n
+        assert fast_exp_iter(a, n) == a ** n
+        assert fast_exp_iter_sol(a, n) == a ** n
     print('done')
---
+++ algorithms/4_DivideAndConquer_Sorting/3_Quicksort/quicksort.py
@@ -0,0 +1,78 @@
+"""
+- Not as clearly a divide and conquer algorithm as merge sort
+- He will do it inplace
+- Why have another algorithm in nlogn?
+    - Quicksort is faster for sorting in memory
+    - Far easier to implement inplace with no extra space usage
+    - Constant factor is faster for quicksort
+- Merge sort is useful still when merging database tables for example
+    - When we want to preserve the sequential access of these tables that aligns with the merge sort implementation
+
+Idea:
+- Bunch of values: 4 3 6 2 5
+- Pick a value in there: 4 (pivot value)
+- Perform an operation where:
+    - Everything less than pivot value on the LHS
+    - Everything higher than the pivot value on the RHS
+- We can recursively apply the same approach to the sub arrays we have created on the LHS and RHS
+    - You are halving the array with each split, so takes log n splits
+    - Takes n operations to do the partitioning
+
+- Can you come up with your own partitioning scheme?
+    - Function that figures out how to move the pivot value to the middle of the array
+
+Example:
+4 3 6 2 5
+3 2 4 6 5
+2 3 4 6 5
+2 3 4 5 6
+"""
+
+import random
+
+def quicksort(n: list[int]) -> list[int]:
+    """
+    Plan:
+    - if len(n) == 1:
+        return n
+    - if len(n) == 2:
+        if n[0] > n[1]:
+            return [n[0], n[1]]
+        return n
+    - (
+        lhs,pivot, rhs
+      ) = partition(n)
+    - return quicksort(lhs) + pivot + quicksort(rhs)
+
+    def partition(n) -> list[int], list[int], list[int]:
+        Lomuto Scheme
+        - Take the first value as the pivot
+        - Iterate over the rest of the array
+            - Maintain an invariant whereby there is a section that is smaller than your pivot and a section which is larger
+        - 4 3 6 2 5
+        - 4 
+
+
+    """
+
+
+random_ints = [random.randint(0, 5000) for _ in range(100_000)]
+TEST_CASES = [
+    ([2, 1], [1, 2]),
+    ([1, 2], [1, 2]),
+    ([3, 1, 2], [1, 2, 3]),
+    ([3, 1, 4, 2], [1, 2, 3, 4]),
+    (random_ints, sorted(random_ints)),
+]
+
+
+if __name__ == "__main__":
+    for case, expected_result in TEST_CASES:
+        try:
+            quicksort(case)
+            assert case == expected_result
+        except AssertionError:
+            print(f"Case: {case}")
+            print(f"Expected Result: {expected_result}")
+            raise
+    print("done")
